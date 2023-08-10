a48cf6bf6cf3d72ebffd418ee8330c9de8fb1271
John Menard Cerezo <johnmenardccerezo@gmail.com>
2023-08-10 16:58:30 +0800

    Add OpenAPI Swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Notes API')
+    .setDescription('NestJS CRUD API with JWT and Passport Authentication')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: { tagsSorter: 'alpha', operationsSorter: 'alpha' },
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
