2dc53bf41eb4340a82bc4059c08ffaa8fc214762
John Menard Cerezo <johnmenardccerezo@gmail.com>
2023-08-11 21:46:40 +0800

    include only specific modules in Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AuthModule } from './auth/auth.module';
+import { NoteModule } from './note/note.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +14,9 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [AuthModule, NoteModule],
+  });
   SwaggerModule.setup('/', app, document, {
     swaggerOptions: { tagsSorter: 'alpha', operationsSorter: 'alpha' },
   });
