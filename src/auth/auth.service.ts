f78325879ab62d34ab2385a82169b2f33cd208af
John Menard Cerezo <johnmenardccerezo@gmail.com>
2023-08-16 00:31:48 +0800

    refactor login jwtService.sign

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -67,7 +67,7 @@ export class AuthService {
       throw new UnauthorizedException('Incorrect email or password.');
     }
 
-    const token = await this.jwtService.signAsync({ id: user._id });
+    const token = this.jwtService.sign({ id: user._id });
 
     return { token };
   }
